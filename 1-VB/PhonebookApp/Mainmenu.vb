7feba8ebb425bd4f60d5b07da941e1e3bd5d2ee7
mariellenolasco <marielle.nolasco@revature.com>
2021-09-30 17:33:50 -0400

    added saving to file and retrieving from file

--- 1-VB/PhonebookApp/Mainmenu.vb
+++ 1-VB/PhonebookApp/Mainmenu.vb
@@ -1,6 +1,10 @@
+Imports System.IO
+Imports System.Text.Json
 Class MainMenu
     Implements IMenu
     dim friends as List(of Contact) = new List(of Contact)
+    dim filename as string = "friends.json"
+    dim jsonstring as string = ""
     Sub Initial() Implements IMenu.Start
         Console.WriteLine("Hello World!")
         dim repeat as Boolean = true
@@ -28,13 +32,29 @@ Class MainMenu
         Console.WriteLine("Phone Number: ")
         dim number as string = Console.ReadLine()
         dim newfriend as Contact = new Contact(name, Int32.Parse(number))
-        friends.Add(newfriend)
+        AddFriend2File(newfriend)
         Console.WriteLine("New Friend Created! " + newfriend.ToString())
     End Sub
     Sub ShowFriends()
         Console.WriteLine("Friend list plus contact info")
-        For Each person As Contact In friends
+        For Each person As Contact In GetContactsFromFile()
             Console.WriteLine(person.ToString())
         Next
     End Sub
+    Sub AddFriend2File(ByVal person as Contact)
+        dim existingContacts as List(of Contact) = GetContacts()
+        existingContacts.Add(person)
+        jsonstring = JsonSerializer.Serialize(existingContacts)
+        File.WriteAllText(filename, jsonstring)
+    End Sub
+    Function GetContactsFromFile() As List(of Contact)
+        Try
+            jsonString = File.ReadAllText(filename)
+            return JsonSerializer.Deserialize(of List(of Contact))(jsonstring)
+        Catch ex As Exception
+            return new List(of Contact)
+        End Try
+        
+    End Function
+    
 End Class
