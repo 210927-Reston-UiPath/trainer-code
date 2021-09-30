1982881b1cd2847910d68a79bd6e80c984ec15fc
mariellenolasco <marielle.nolasco@revature.com>
2021-09-30 17:13:31 -0400

    added show friends

--- 1-VB/PhonebookApp/Mainmenu.vb
+++ 1-VB/PhonebookApp/Mainmenu.vb
@@ -1,17 +1,21 @@
 Class MainMenu
     Implements IMenu
+    dim friends as List(of Contact) = new List(of Contact)
     Sub Initial() Implements IMenu.Start
         Console.WriteLine("Hello World!")
         dim repeat as Boolean = true
         Do While repeat
             Console.WriteLine("What would you like to do?")
             Console.WriteLine("[0] Add a friend")
+            Console.WriteLine("[1] Show all friends")
             Console.WriteLine("[x] Exit")
             Dim input as string = Console.ReadLine()
             Select Case input
                 Case "0"
                     Console.WriteLine("Hello")
                     AddFriend()
+                Case "1"
+                    ShowFriends()
                 Case "x"
                     Console.WriteLine("Goodbye")
                     repeat = false
@@ -24,7 +28,15 @@ Class MainMenu
         Console.WriteLine("Phone Number: ")
         dim number as string = Console.ReadLine()
         dim newfriend as Contact = new Contact(name, Int32.Parse(number))
+        friends.Add(newfriend)
         Console.WriteLine("New Friend Created! " + newfriend.ToString())
     End Sub
+    Sub ShowFriends()
+        Console.WriteLine("Friend list plus contact info")
+        For Each person As Contact In friends
+            Console.WriteLine(person.ToString())
+        Next
+    End Sub
+    
     
 End Class
