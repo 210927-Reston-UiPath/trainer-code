dc1b1cd2f592b17832f35dcefa14f30e0d64297e
mariellenolasco <marielle.nolasco@revature.com>
2021-10-26 12:52:25 -0400

    Email Automations

--- 4-Web/MoreWebStuff/index.js
+++ 4-Web/MoreWebStuff/index.js
@@ -1,5 +1,5 @@
 function addBorder()
 {
     document.getElementById('cat').setAttribute('border', '2px');
-    document.querySelector('#catpic p').setAttribute('style', 'color:grey');
+    document.querySelector('#cat + p').setAttribute('style', 'border:solid');
 }
